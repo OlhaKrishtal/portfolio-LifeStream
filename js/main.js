c361258ee04d0179696816eb7be054f6bb72b9d7
OlhaKrishtal <olyamakarenko65@gmail.com>
2021-11-22 00:11:50 +0200

    “comment”

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $('.benefits__slider-inner').slick({
+        dots: false,
+        arrows: false,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+      });
+});
